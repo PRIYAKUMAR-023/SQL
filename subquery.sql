9f3b6eb60a06d91f6f2e86344f71e0e76c522fc6
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-10-07 21:53:17 +0530

    Add files via upload

---
+++ subquery.sql
@@ -0,0 +1,15 @@
+use company;
+select*from db_employee;
+select *from db_dept;
+
+select department_id,max(salary) from db_employee group by department_id;
+select *from db_employee where (department_id,salary)in (select department_id,max(salary) from db_employee group by department_id);
+
+
+select e.first_name ,e.last_name,e.salary,d.department from db_employee as e left join db_dept as d on d.department_id=e.department_id;
+
+select *from db_employee  where (select e.first_name ,e.last_name,e.salary,d.department from db_employee as e left join db_dept as d on d.department_id=e.department_id);
+
+update db_employee set gender='M' where gender is null;
+set sql_safe_updates=0;
+update db_employee set gender='F' where id=21002;
