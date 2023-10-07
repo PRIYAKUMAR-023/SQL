246971d8d3b47e8ffb89097247868ab4439d89a9
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-10-07 21:51:07 +0530

    DATABASES

---
+++ Databases.sql
@@ -0,0 +1,11 @@
+-- create data base --
+create database study;
+
+-- drop the data base-- 
+drop database sys;
+
+-- which database we want store the data--
+use study;
+
+-- show all  the databases--
+show databases;
